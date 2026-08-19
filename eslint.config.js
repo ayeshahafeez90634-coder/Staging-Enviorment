aac96deb5481ad56f118603fe22b2fdef76df712
Ayesha Hafeez <ayeshahafeez90634@gmail.com>
2026-08-19 16:26:59 +0500

    fix: configure Node globals for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,22 @@ const { defineConfig } = require("eslint/config");
 module.exports = defineConfig([
     {
         files: ["**/*.js"],
+
         languageOptions: {
             ecmaVersion: 2021,
-            sourceType: "commonjs"
+            sourceType: "commonjs",
+
+            globals: {
+                process: "readonly",
+                console: "readonly",
+                __dirname: "readonly",
+                __filename: "readonly",
+                require: "readonly",
+                module: "readonly",
+                exports: "readonly"
+            }
         },
+
         rules: {
             "no-unused-vars": "error",
             "no-undef": "error"
